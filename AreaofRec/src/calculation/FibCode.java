f7b33ad8c570c8bd6ea7a62e3bb31586120a1e49
Sharmi Srinivas <Sharmi Srinivas@DESKTOP-HC4MJDS>
2020-03-14 09:10:45 -0700

    fib series code

---
+++ AreaofRec/src/calculation/FibCode.java
@@ -0,0 +1,36 @@
+package calculation;
+
+import java.util.Scanner;
+
+public class FibCode {
+	static int f1=0;
+	static int f2=1;		
+	static int f3;
+	
+	static void fibSeries(int input) {
+		System.out.println(f1+"\n"+f2);
+		for(int i=2;i<=input;i++) {
+			f3=f1+f2;
+			//if(f3<=input)/*in case to get fib series num upto the between range*/
+			System.out.println(f3 +" ");
+			f1=f2;
+			f2=f3;
+			}
+		
+		
+		
+	}
+	
+
+	public static void main(String[] args) {
+		System.out.println("Enter the range of fib series");
+		Scanner scan = new Scanner(System.in);
+		int input = scan.nextInt();
+		System.out.println("The fib series of "+input+ " is:");
+		fibSeries(input);
+		scan.close();
+		
+		
+	}
+
+}
